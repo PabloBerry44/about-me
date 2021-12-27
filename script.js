0df04157ea1e6696a0e82ea151eb3f62aa52d5ae
PabloBerry44 <pawel.borowiec22@gmail.com>
2021-12-27 13:09:09 +0100

    touch

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const arrow = document.querySelector('.arrow')
-arrow.addEventListener('click', () => window.scrollBy({
-    top: 200,
-    behavior: 'smooth',
-}));
+const cards = document.querySelectorAll('.card')
+
+cards.forEach(cards => {
+    cards.addEventListener("mouseenter", move)
+    cards.addEventListener("mouseleave", move)
+    
+    cards.addEventListener("touchstart", move)
+    cards.addEventListener("touchend", move)
+
+    function move(){
+        cards.classList.toggle('card-move')
+    }
+});

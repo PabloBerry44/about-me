bfde79cf72378accadbd52fa21e387126f0649cd
PabloBerry44 <pawel.borowiec22@gmail.com>
2021-12-27 18:03:06 +0100

    image and transition

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+hero = document.querySelector('.inner-hero')
+hero.style.transform='none'
+
 const cards = document.querySelectorAll('.card')
-
 //TRIGGER FOR DESKTOP
-
 for(var i = 0; i < cards.length; i++){
     cards[i].addEventListener('mouseenter', function(){
       // remove card-move class for all elements
@@ -12,9 +13,7 @@ for(var i = 0; i < cards.length; i++){
       this.classList.add('card-move');
     });
   }
-
 //TRIGGER FOR MOBILE DEVICES AND TOUCH SCREENS
-
   for(var i = 0; i < cards.length; i++){
     cards[i].addEventListener('touchstart', function(){
       // remove card-move class for all elements
